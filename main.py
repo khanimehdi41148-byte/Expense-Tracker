c3376a6e1c66f7b45756f9b28ec735221f559a04
mehdi-khani <khanimehdi41148@gmail.com>
2026-08-08 19:10:54 +0330

    Implement remove expense feature

--- main.py
+++ main.py
@@ -20,6 +20,19 @@ class ExpenseManager:
 
     def add_expense(self, expense):
         self.expenses.append(expense)
-        
+
+    def remove_expense(self, index):
+        try:
+            index = int(index) -1
+        except ValueError:
+            print("invalid index! enter number...")
+            return
+        if 0 <= index < len(self.expenses):
+            removed = self.expenses.pop(index)
+            print(f"{removed.description} deleted")
+            return
+        else:
+            print("Expense not found")
+            
 
     
